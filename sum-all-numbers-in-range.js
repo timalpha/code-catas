07de36c53c34f5e4510a4ecb3cd14d4e6d66c224
Tim Alpha <timalpha@users.noreply.github.com>
2016-11-25 13:37:56 +0100

    Fix indentation

--- sum-all-numbers-in-range.js
+++ sum-all-numbers-in-range.js
@@ -1,12 +1,12 @@
 function sumAll(arr) {
-  var max = Math.max(...arr);
-  var min = Math.min(...arr);
-  var sum = 0;
-  
-  for (var i = min; i <= max; i++) {
-    sum += i;
-  }
-  return sum;
+    var max = Math.max(...arr);
+    var min = Math.min(...arr);
+    var sum = 0;
+
+    for (var i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 }
 
 sumAll([1, 4]);
